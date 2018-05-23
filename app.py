6204a570e9809886e7dff8744b36ed4fbdffc39d
chao <blue14753@gmail.com>
2018-05-23 17:28:19 +0800

    Init

--- app.py
+++ app.py
@@ -28,8 +28,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	line_bot_api.reply_message(
-		event.reply_token,
-		TextSendMessage(text=event.message.text))
+    message = TextsendMessage(text='Hello')
+    line_bot_api.reply_message(event.reply_token,message)
+	#line_bot_api.reply_message(
+		#event.reply_token,
+		#TextSendMessage(text=event.message.text))
 if __name__ == "__main__":
 	app.run()
